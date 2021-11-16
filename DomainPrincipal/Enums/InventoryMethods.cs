ee2153b7fe126eeb7b0cc12c63f4cd0cb6095a48
Dierish05 <85861227+Dierish05@users.noreply.github.com>
2021-11-16 14:06:25 -0600

    Add files via upload

---
+++ DomainPrincipal/Enums/InventoryMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainPrincipal.Enums
+{
+    public enum InventoryMethod
+    {
+        PEPS,
+        UEPS,
+        SingleAverage
+    }
+}
